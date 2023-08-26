73896ba10858da6072761d84f766509c17d95d7a
natnael340 <natnaeltilahun157@gmail.com>
2023-08-26 10:54:58 +0300

    directory update

---
+++ myblog/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^post/', include("posts.urls")),
+    url(r'^pro/', include("profiles.urls")),
+    url(r'^', include("frontend.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
