2a7bf9e2d9794303e5bdb00e7647183c745213a2
natnael340 <natnaeltilahun157@gmail.com>
2023-08-26 11:09:12 +0300

    directory update

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,15 +3,16 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^post/', include("posts.urls")),
-    url(r'^pro/', include("profiles.urls")),
-    url(r'^', include("frontend.urls")),
+    url(r"^admin/", admin.site.urls),
+    url(r"^post/", include("posts.urls")),
+    url(r"^pro/", include("profiles.urls")),
+    url(r"^", include("frontend.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
